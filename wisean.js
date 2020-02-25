d17db5ff3e178098632c40d337c133ca8efb2507
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-02-25 11:39:14 -0700

    Delete wisean.js

--- wisean.js
+++
@@ -1,2 +0,0 @@
-var lol = window.open("https://www.youtube.com/watch?v=buc64u6Q_oA&authuser=0", "MsgWindow", "width=1000,height=1000");
-lol.document.write("<iframe height='100%' width='100%' src='https://www.youtube.com/watch?v=buc64u6Q_oA&authuser=0'></iframe>");
